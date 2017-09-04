0f8748616bc263bad7b3f25296fc2983460e5ea0
Dian Tai <diantai@gmail.com>
2017-09-04 00:51:56 +0000

    moved user_id and username columns from games to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903223300) do
+ActiveRecord::Schema.define(version: 20170903232917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,7 @@ ActiveRecord::Schema.define(version: 20170903223300) do
     t.integer  "black_player"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "user_id"
     t.index ["black_player"], name: "index_games_on_black_player", using: :btree
-    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
     t.index ["white_player", "black_player"], name: "index_games_on_white_player_and_black_player", using: :btree
   end
 
@@ -51,8 +49,11 @@ ActiveRecord::Schema.define(version: 20170903223300) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "user_id"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["user_id"], name: "index_users_on_user_id", using: :btree
   end
 
 end
