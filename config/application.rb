cf5312dc018f80cf8bce9cd57c3ecab189feed77
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-06 05:48:56 +0000

    Added class for individual pieces

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Chess
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # This adds the pieces folder to autoload so they can be used in as a piece type
+    config.autoload_paths += %W(#{config.root}/app/models/pieces)
   end
 end
