5106b7359b4adefe75b9236f48d6420503337bec
KevinKawai <kevin.fredricks@gmail.com>
2017-09-09 09:36:41 +0900

    Fixed some spelling in test description

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
   describe "Piece#is_obstructed? method" do
-    it "should successfully detect is a move is obstructed (horizontl)" do
+    it "should successfully detect if a move is obstructed (horizontal)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:1,y:1
       pawn2 = FactoryGirl.create(:piece, type:Pawn,x:5) #x:5,y:1
 
       expect(pawn1.is_obstructed?(10,1)).to eq(true)
     end
 
-    it "should successfully detect is a move is obstructed (vertical)" do
+    it "should successfully detect if a move is obstructed (vertical)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:1,y:1
       pawn2 = FactoryGirl.create(:piece, type:Pawn,y:5) #x:1,y:5
 
       expect(pawn1.is_obstructed?(1,10)).to eq(true)
     end
     
-    it "should successfully detect is a move is obstructed (diagnal)" do
+    it "should successfully detect if a move is obstructed (diagonal)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:1,y:1
       pawn2 = FactoryGirl.create(:piece, type:Pawn,x:3,y:3) #x:3,y:3
 
