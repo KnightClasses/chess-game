9caf64a5ddaa14a56470ab6a0f34141e5f23a65e
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-09-14 18:38:35 +0000

    Use enum and valid tests.

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,33 +6,33 @@ class Game < ApplicationRecord
 
   def populate_game!
     1.upto(8) do |x_pos|
-      Pawn.create(game_id: id, x: x_pos, y: 2, color: 0)
+      Pawn.create(game_id: id, x: x_pos, y: 2, color: "white")
     end
 
     1.upto(8) do |x_pos|
-      Pawn.create(game_id: id, x: x_pos, y: 7, color: 1)
+      Pawn.create(game_id: id, x: x_pos, y: 7, color: "black")
     end
 
-    Rook.create(game_id: id, x: 1, y: 1, color: 0)
-    Rook.create(game_id: id, x: 8, y: 1, color: 0)
-    Rook.create(game_id: id, x: 1, y: 8, color: 1)
-    Rook.create(game_id: id, x: 8, y: 8, color: 1)
+    Rook.create(game_id: id, x: 1, y: 1, color: "white")
+    Rook.create(game_id: id, x: 8, y: 1, color: "white")
+    Rook.create(game_id: id, x: 1, y: 8, color: "black")
+    Rook.create(game_id: id, x: 8, y: 8, color: "black")
 
-    Knight.create(game_id: id, x: 7, y: 1, color: 0)
-    Knight.create(game_id: id, x: 2, y: 1, color: 0)
-    Knight.create(game_id: id, x: 7, y: 8, color: 1)
-    Knight.create(game_id: id, x: 2, y: 8, color: 1)
+    Knight.create(game_id: id, x: 7, y: 1, color: "white")
+    Knight.create(game_id: id, x: 2, y: 1, color: "white")
+    Knight.create(game_id: id, x: 7, y: 8, color: "black")
+    Knight.create(game_id: id, x: 2, y: 8, color: "black")
 
-    Bishop.create(game_id: id, x: 6, y: 1, color: 0)
-    Bishop.create(game_id: id, x: 3, y: 1, color: 0)
-    Bishop.create(game_id: id, x: 6, y: 8, color: 1)
-    Bishop.create(game_id: id, x: 3, y: 8, color: 1)
+    Bishop.create(game_id: id, x: 6, y: 1, color: "white")
+    Bishop.create(game_id: id, x: 3, y: 1, color: "white")
+    Bishop.create(game_id: id, x: 6, y: 8, color: "black")
+    Bishop.create(game_id: id, x: 3, y: 8, color: "black")
 
-    Queen.create(game_id: id, x: 4, y: 1, color: 0)
-    Queen.create(game_id: id, x: 4, y: 8, color: 1)
+    Queen.create(game_id: id, x: 4, y: 1, color: "white")
+    Queen.create(game_id: id, x: 4, y: 8, color: "black")
 
-    King.create(game_id: id, x: 5, y: 1, color: 0)
-    King.create(game_id: id, x: 5, y: 8, color: 1)
+    King.create(game_id: id, x: 5, y: 1, color: "white")
+    King.create(game_id: id, x: 5, y: 8, color: "black")
   end
 
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Piece, type: :model do
   describe "Piece#is_obstructed? method" do
     it "should successfully detect if a move is obstructed (horizontal)" do
-      pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
-      pawn2 = FactoryGirl.create(:piece, type:Pawn,x:5) #x:5,y:3
+      rook1 = FactoryGirl.create(:piece, type:Rook) #x:3,y:3
+      rook2 = FactoryGirl.create(:piece, type:Rook,x:5) #x:5,y:3
 
-      expect(pawn1.is_obstructed?(6,3)).to eq(true)
+      expect(rook1.is_obstructed?(6,3)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (horizontal)" do
@@ -16,29 +16,29 @@ RSpec.describe Piece, type: :model do
     end
 
     it "should successfully detect if a move is obstructed (vertical)" do
-      pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
-      pawn2 = FactoryGirl.create(:piece, type:Pawn,y:5) #x:3,y:5
+      rook1 = FactoryGirl.create(:piece, type:Rook) #x:3,y:3
+      rook2 = FactoryGirl.create(:piece, type:Rook,y:5) #x:3,y:5
 
-      expect(pawn1.is_obstructed?(3,6)).to eq(true)
+      expect(rook1.is_obstructed?(3,6)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (vertical)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
 
-      expect(pawn1.is_obstructed?(3,6)).to eq(false)
+      expect(pawn1.is_obstructed?(3,4)).to eq(false)
     end
 
     it "should successfully detect if a move is obstructed (diagonal)" do
-      pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
-      pawn2 = FactoryGirl.create(:piece, type:Pawn,x:4,y:4) #x:4,y:4
+      bishop1 = FactoryGirl.create(:piece, type:Bishop) #x:3,y:3
+      bishop2 = FactoryGirl.create(:piece, type:Bishop,x:4,y:4) #x:4,y:4
 
-      expect(pawn1.is_obstructed?(5,5)).to eq(true)
+      expect(bishop1.is_obstructed?(5,5)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (diagonal)" do
-      pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
+      bishop1 = FactoryGirl.create(:piece, type:Bishop) #x:3,y:3
 
-      expect(pawn1.is_obstructed?(5,5)).to eq(false)
+      expect(bishop1.is_obstructed?(5,5)).to eq(false)
     end
 
     it "should successfully detect if a move is illegal (neither horizontal, vertical, nor diagonal)" do
