9f844a9dbd7bacbc6ebb457973f04645b908a069
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-14 08:26:18 +0000

    Added 3 tests for populate

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Game, type: :model do
       expect(game.length).to eq(1)
     end
   end
+  describe "game#populate_game!" do
+    it "should create 32 pieces when a game gets created" do
+      FactoryGirl.create(:game)
+
+      expect(Piece.count).to eq(32)
+    end
+    it "should create 6 unique pieces when a game gets created" do
+      FactoryGirl.create(:game)
+
+      expect((Piece.group(:type).count).length).to eq(6)
+    end
+
+    it "should create 16 pieces on both the black side and white side" do
+      FactoryGirl.create(:game)
+
+      expect((Piece.group(:color).count)["white"]).to eq(16)
+      expect((Piece.group(:color).count)["black"]).to eq(16)
+    end
+  end
 end
