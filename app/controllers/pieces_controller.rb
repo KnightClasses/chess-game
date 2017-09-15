9d3d91c0434bf80802091b4207d4fa6d9df26db5
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-09-15 17:54:41 +0000

    Update tests, controller, route. In progress.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,9 +1,18 @@
 class PiecesController < ApplicationController
   def show
-    @piece = Piece.find(params[:id])
+    @game = Game.find(params[:game_id])
+    @piece = @game.pieces.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:game_id])
+    @piece = @game.pieces.find(params[:id])
 
+    @piece.update_attributes(piece_params)
+  end
+
+  private
+  def piece_params
+    params.require(:piece).permit(:color, :type, :x, :y)
   end
 end
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -3,20 +3,24 @@ require 'rails_helper'
 RSpec.describe PiecesController, type: :controller do
   describe "pieces#show action" do 
     it "should successfully show the page" do 
-      get :show
+      user = FactoryGirl.create(:user)
+      sign_in user
+      game = FactoryGirl.create(:game)
+      piece = Piece.last
+      get :show, params: { game_id: game.id, id: piece.id }
       expect(response).to have_http_status(:success)
     end
   end
-
 
   describe "pieces#update action" do 
     it "should allow a user to successfully move (update x/y) a piece" do 
       user = FactoryGirl.create(:user)
       sign_in user
       game = FactoryGirl.create(:game)
+      piece = Piece.last
       req_x = 4
       req_y = 4
-      patch :update, params: { game_id: game.id, piece: { type: Bishop, x: req_x, y: req_y } }
+      patch :update, params: { game_id: game.id, id: piece.id, piece: { x: req_x, y: req_y } }
       game.reload
       expect(piece.x).to eq(req_x)
       expect(piece.y).to eq(req_y)
