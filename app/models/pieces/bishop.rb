1e56d2d89eaaeab3501529b23cf9cacc8f41318b
Justin Rice <jsrice7391@gmail.com>
2017-09-20 19:22:44 +0000

    Bishop is_valid?

--- app/models/pieces/bishop.rb
+++ app/models/pieces/bishop.rb
@@ -3,4 +3,28 @@ class Bishop < Piece
   def check
     return "This is the bishop"
   end
+
+  def is_valid(move_x, move_y)
+    horizontal = []
+    vertical = []
+    point = [move_x, move_y]
+
+
+    for i in 1..8 
+        horizontal << [i, self.y]    
+    end
+
+    for i in 1..8
+        vertical << [self.x, i]
+    end
+
+    all = horizontal + vertical
+
+
+    if all.include?(point)
+        return true
+    else
+      return false
+    end
+  end
 end
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe Bishop, type: :model do
       expect(piece.check).to eq(expected)
     end
   end
+
+  describe "Bishop#is_valid?" do
+    it "should tell us if the bishop Piece is moving to a valid spot" do
+      FactoryGirl.create(:piece, type: Bishop, x: 1, y:1)
+      piece = Piece.last
+
+      expect(piece.is_valid(1, 6)).to eq(true)
+      expect(piece.is_valid(1, 2)).to eq(true)
+      expect(piece.is_valid(1, 3)).to eq(true)
+    end
+
+    it "should tell us that the move is invalid" do
+      FactoryGirl.create(:piece, type: Bishop, x:1, y:1)
+      piece = Piece.last
+      
+      
+      expect(piece.is_valid(7,8)).to eq(false)
+      expect(piece.is_valid(6,4)).to eq(false)
+    end
 end
+
+end
+
