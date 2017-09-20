2cc2ef514866ae3ae2f32d0fbd18d78cd1aaa1a5
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-09-20 20:07:22 +0000

    Simplify bishop is_valid? method and changed tests.

--- app/models/pieces/bishop.rb
+++ app/models/pieces/bishop.rb
@@ -4,27 +4,9 @@ class Bishop < Piece
     return "This is the bishop"
   end
 
-  def is_valid(move_x, move_y)
-    horizontal = []
-    vertical = []
-    point = [move_x, move_y]
+  def is_valid?(req_x, req_y)
+    return true if (self.x - req_x).abs == (self.y - req_y).abs 
 
-
-    for i in 1..8 
-        horizontal << [i, self.y]    
-    end
-
-    for i in 1..8
-        vertical << [self.x, i]
-    end
-
-    all = horizontal + vertical
-
-
-    if all.include?(point)
-        return true
-    else
-      return false
-    end
+    return false
   end
 end
