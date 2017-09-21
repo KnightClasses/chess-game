1697dea82e897a53193ad04d228746c8b166d6fb
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-09-20 21:22:07 -0400

    Update queen.rb

--- app/models/pieces/queen.rb
+++ app/models/pieces/queen.rb
@@ -4,22 +4,11 @@ class Queen < Piece
     return "This is the queen"
   end
 
-<<<<<<< HEAD
   def is_valid?(req_x, req_y)
-    if (req_x == self.x || req_y == self.y) || ((req_x - self.x).abs == (req_y - self.y).abs)
+    if (req_x == self.x || req_y == self.y) || ((req_x - self.x).abs == (req_y - self.y).abs))
         return true
     else
         return false
     end
   end
-=======
-   def is_valid (req_x, req_y)
-        if (req_x == self.x || req_y == self.y) || (req_x - self.x === req_y - self.y) 
-            return true
-        else
-            return false
-      end
-    end
->>>>>>> 3bc68afda86b60ed3d1b5722f23318d7ac9c93db
-
 end
