ed1a90b376d24b2fcad1e7730b5ea31e0b21d2f7
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-09-26 16:55:22 +0000

    Delete pieces show page test.

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-  describe "pieces#show action" do 
-    it "should successfully show the page" do 
-      user = FactoryGirl.create(:user)
-      sign_in user
-      game = FactoryGirl.create(:game)
-      piece = Piece.last
-      get :show, params: { game_id: game.id, id: piece.id }
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "pieces#update action" do 
     it "should allow a user to successfully move (update x/y) a piece" do 
       user = FactoryGirl.create(:user)
