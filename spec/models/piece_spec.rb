99203de2371e9114519631163be03c4dc17cbabc
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-02 16:05:34 +0000

    Limit piece queries to current game in piece.rb.

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -6,45 +6,45 @@ RSpec.describe Piece, type: :model do
       rook1 = FactoryGirl.create(:piece, type:Rook) #x:3,y:3
       rook2 = FactoryGirl.create(:piece, type:Rook,x:5,game_id: rook1.game.id) #x:5,y:3
 
-      expect(rook1.is_obstructed?(6,3,rook1.game.id)).to eq(true)
+      expect(rook1.is_obstructed?(6,3)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (horizontal)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
 
-      expect(pawn1.is_obstructed?(2,3,pawn1.game.id)).to eq(false)
+      expect(pawn1.is_obstructed?(2,3)).to eq(false)
     end
 
     it "should successfully detect if a move is obstructed (vertical)" do
       rook1 = FactoryGirl.create(:piece, type:Rook) #x:3,y:3
       rook2 = FactoryGirl.create(:piece, type:Rook,y:5,game_id: rook1.game.id) #x:3,y:5
 
-      expect(rook1.is_obstructed?(3,6,rook1.game.id)).to eq(true)
+      expect(rook1.is_obstructed?(3,6)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (vertical)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
 
-      expect(pawn1.is_obstructed?(3,4,pawn1.game.id)).to eq(false)
+      expect(pawn1.is_obstructed?(3,4)).to eq(false)
     end
 
     it "should successfully detect if a move is obstructed (diagonal)" do
       bishop1 = FactoryGirl.create(:piece, type:Bishop) #x:3,y:3
       bishop2 = FactoryGirl.create(:piece, type:Bishop,x:4,y:4,game_id: bishop1.game.id) #x:4,y:4
 
-      expect(bishop1.is_obstructed?(5,5,bishop1.game.id)).to eq(true)
+      expect(bishop1.is_obstructed?(5,5)).to eq(true)
     end
 
     it "should successfully detect if a move is not obstructed (diagonal)" do
       bishop1 = FactoryGirl.create(:piece, type:Bishop) #x:3,y:3
 
-      expect(bishop1.is_obstructed?(5,5,bishop1.game.id)).to eq(false)
+      expect(bishop1.is_obstructed?(5,5)).to eq(false)
     end
 
     it "should successfully detect if a move is illegal (neither horizontal, vertical, nor diagonal)" do
       pawn1 = FactoryGirl.create(:piece, type:Pawn) #x:3,y:3
 
-      expect(pawn1.is_obstructed?(2,5,pawn1.game.id)).to eq("invalid move")
+      expect(pawn1.is_obstructed?(2,5)).to eq("invalid move")
     end
   end
 end
