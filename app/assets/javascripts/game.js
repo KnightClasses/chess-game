4e99dcd1c2df91c599e9be4f7830d7a071994a2a
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-06 17:26:38 +0000

    Creating buttons in JS for pawn promotion.

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -22,10 +22,30 @@ $(document).ready(function () {
         });
         var x = $(event.target).data("x");
         var y = $(event.target).data("y");
+        var type = $(event.target).data("type");
+        var color = $(event.target).data("color");
         var piece = {
           x: x,
-          y: y
+          y: y,
+          type: type,
+          color: color
         }
+        if (( type == "pawn" && y == 1 ) || ( type == "pawn" && y == 8 )) {
+          alert("promote pawn?");
+          // 1. Create the button
+          var button = document.createElement("button");
+          button.innerHTML = "Do Something";
+
+          // 2. Append somewhere
+          var body = document.getElementsByClassName("black messages")[0];
+          body.appendChild(button);
+
+          // 3. Add event handler
+          button.addEventListener ("click", function() {
+            alert("did something");
+          });
+        }
+
         $.ajax({
           type: 'PATCH',
           url: ui.draggable.data('update-url'),
