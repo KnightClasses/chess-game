314109fe35fde1ac72f1735d354581007e486b72
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-09 01:23:28 +0000

    Logic for diagonal pawn capture. Need to fix test.

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -27,5 +27,19 @@ RSpec.describe Pawn, type: :model do
       expect(pawn1.is_valid?(4, 2)).to eq(false)
       expect(pawn1.is_valid?(3, 5)).to eq(false)
     end
+
+    it "should successfully detect if a move is valid for diagonal captures" do 
+      game = FactoryGirl.create(:game)
+      pawn1 = FactoryGirl.create(:piece, type: Pawn, color: 0, x:3, y:2, game_id: game.id)
+      pawn2 = FactoryGirl.create(:piece, type: Pawn, color: 1, x:4, y:3, game_id: game.id)
+      pawn3 = FactoryGirl.create(:piece, type: Pawn, color: 1, x:2, y:3, game_id: game.id)
+
+      puts ""
+      puts pawn1.inspect
+      puts pawn1.type
+
+      expect(pawn1.is_valid?(4, 3)).to eq(true)
+      expect(pawn1.is_valid?(2, 3)).to eq(true)
+    end
   end
 end
