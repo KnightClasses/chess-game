166a900fe59892b62296a63d3be265c53c4127e7
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-09 01:51:34 +0000

    Allow straight move when piece is not present.

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -7,12 +7,12 @@ class Pawn < Piece
   def is_valid?(req_x, req_y)
     if self.color == "white"
       if self.y == 2
-        return true if (req_x == self.x && req_y == self.y + 2)
-        return true if (req_x == self.x && req_y == self.y + 1)
+        return true if (req_x == self.x && req_y == self.y + 2) && !(game.pieces.find_by(x: self.x, y: self.y + 2).present?)
+        return true if (req_x == self.x && req_y == self.y + 1) && !(game.pieces.find_by(x: self.x, y: self.y + 1).present?)
         return true if req_x == self.x + 1 && req_y == self.y + 1 && (game.pieces.find_by(x: self.x + 1, y: self.y + 1).present?)
         return true if req_x == self.x - 1 && req_y == self.y + 1 && (game.pieces.find_by(x: self.x - 1, y: self.y + 1).present?)
       else
-        return true if (req_x == self.x && req_y == self.y + 1) 
+        return true if (req_x == self.x && req_y == self.y + 1) && !(game.pieces.find_by(x: self.x + 1, y: self.y + 1).present?)
         return true if req_x == self.x + 1 && req_y == self.y + 1 && (game.pieces.find_by(x: self.x + 1, y: self.y + 1).present?)
         return true if req_x == self.x - 1 && req_y == self.y + 1 && (game.pieces.find_by(x: self.x - 1, y: self.y + 1).present?)
       end
@@ -20,12 +20,12 @@ class Pawn < Piece
     
     if self.color == "black"
       if self.y == 7
-        return true if (req_x == self.x && req_y == self.y - 2)
-        return true if (req_x == self.x && req_y == self.y - 1)
+        return true if (req_x == self.x && req_y == self.y - 2) && !(game.pieces.find_by(x: self.x , y: self.y - 2).present?)
+        return true if (req_x == self.x && req_y == self.y - 1) && !(game.pieces.find_by(x: self.x , y: self.y - 1).present?)
         return true if req_x == self.x + 1 && req_y == self.y - 1 && (game.pieces.find_by(x: self.x + 1, y: self.y - 1).present?)
         return true if req_x == self.x - 1 && req_y == self.y - 1 && (game.pieces.find_by(x: self.x - 1, y: self.y - 1).present?)
       else
-        return true if (req_x == self.x && req_y == self.y - 1)
+        return true if (req_x == self.x && req_y == self.y - 1) && !(game.pieces.find_by(x: self.x, y: self.y - 1).present?)
         return true if req_x == self.x + 1 && req_y == self.y - 1 && (game.pieces.find_by(x: self.x + 1, y: self.y - 1).present?)
         return true if req_x == self.x - 1 && req_y == self.y - 1 && (game.pieces.find_by(x: self.x - 1, y: self.y - 1).present?)
       end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -37,12 +37,18 @@ RSpec.describe Pawn, type: :model do
       pawn2 = Piece.find_by("x = 4 AND y = 3 AND game_id = ? AND type = 'Pawn'", game.id)
       pawn3 = Piece.find_by("x = 2 AND y = 3 AND game_id = ? AND type = 'Pawn'", game.id)
 
-      puts ""
-      puts pawn1.inspect
-      puts pawn1.type
-
       expect(pawn1.is_valid?(4, 3)).to eq(true)
       expect(pawn1.is_valid?(2, 3)).to eq(true)
+    end
+
+    it "should successfully detect if a move is not valid for straight captures" do 
+      game = FactoryGirl.create(:game)
+      FactoryGirl.create(:piece, type: Pawn, color: "white", x:3, y:2, game_id: game.id)
+      FactoryGirl.create(:piece, type: Pawn, color: "black", x:3, y:3, game_id: game.id)
+      pawn1 = Piece.find_by("x = 3 AND y = 2 AND game_id = ? AND type = 'Pawn'", game.id)
+      pawn2 = Piece.find_by("x = 3 AND y = 3 AND game_id = ? AND type = 'Pawn'", game.id)
+
+      expect(pawn1.is_valid?(3, 3)).to eq(false)
     end
   end
 end
