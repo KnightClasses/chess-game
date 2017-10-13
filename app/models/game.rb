7b1fb3ebbda37be0ed2d773aaa95ecf07e159514
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-10-13 14:14:00 +0000

    Double check tests. Remove multiple pieces logic where unnecessary.

--- app/models/game.rb
+++ app/models/game.rb
@@ -82,9 +82,8 @@ class Game < ApplicationRecord
       return false if (
         player_turn_color == color &&
         (king_can_move_and_prevent_checkmate?(color) ||
-        (threatening_pieces?(color).count == 1 &&
         (threatening_piece_may_be_blocked_by_teammate?(color) ||
-        (threatening_piece_may_be_captured_by_teammate?(color) ) ) ) ) )
+        (threatening_piece_may_be_captured_by_teammate?(color) ) ) ) )
       return true
     end
 
@@ -110,6 +109,78 @@ class Game < ApplicationRecord
       end
     end
     return false ## no such safe spot exists for the king to move itself to
+  end
+
+  def threatening_pieces?(color) ## finds all pieces that are holding the king(color) in check
+    if color == 'white'
+      opposing_color = 'black'
+    else
+      opposing_color = 'white'
+    end
+
+    king = self.pieces.find_by(type: "King", color: color)
+    threatening_pieces = []
+
+    #iterate through the opposing pieces for check on the king
+    pieces = self.pieces.where(color: opposing_color, active: true).to_a
+    pieces.each do |piece|
+      if piece.valid_move?(king.x, king.y)
+        threatening_pieces << piece
+      end
+    end
+    return threatening_pieces
+  end
+
+  def threatening_piece_may_be_captured_by_teammate?(color)
+    # if there is more than 1 threatening piece, it doesn't matter if you can capture one of them
+    # because the other one(s) can capture the king on the next move
+    return false if threatening_pieces?(color).count > 1
+
+    king = self.pieces.find_by(type: "King", color: color)
+    pieces = king.game.find_in_game(color: color, active: true).to_a
+    threatening_piece = threatening_pieces?(color)[0]
+
+    pieces.each do |piece|
+      if piece.valid_move?(threatening_piece.x, threatening_piece.y)
+        return true
+      end
+    end
+    return false
+  end
+
+  def threatening_piece_may_be_blocked_by_teammate?(color)
+    # if there is more than 1 threatening piece, it doesn't matter if you can block one of them
+    # because the other one(s) can capture the king on the next move
+    return false if threatening_pieces?(color).count > 1
+
+    king = self.pieces.find_by(type: "King", color: color)
+    teammate_pieces = self.pieces.where(color: color, active: true).where.not(type: "King").to_a
+    threatening_piece = threatening_pieces?(color)[0]
+
+    # check each capture path for this threatening piece
+    threatening_piece.capture_path.values.each do |path|
+
+      # find which path the king is on
+      if path.include?([king.x, king.y])
+        king_is_on_this_path = path
+      else
+        next
+      end
+
+      # go through each of pieces on your own team
+      teammate_pieces.each do |teammate_piece|
+
+        # go through each of the cells between the threatening piece and the king
+        king_is_on_this_path.each do |cell|
+
+          # if a teammate piece may move to any of the cells along this path, the threat CAN be BLOCKED
+          return true if teammate_piece.valid_move?(cell[0], cell[1])
+        end
+      end
+
+      # the threat cannot be blocked
+      return false
+    end
   end
 
   def threatening_pieces_directional_adjustment?(color)
@@ -139,80 +210,4 @@ class Game < ApplicationRecord
     directional_adjustment
   end
 
-  def threatening_pieces?(color) ## finds all pieces that are holding the king(color) in check
-    if color == 'white'
-      opposing_color = 'black'
-    else
-      opposing_color = 'white'
-    end
-
-    king = self.pieces.find_by(type: "King", color: color)
-    threatening_pieces = []
-
-    #iterate through the opposing pieces for check on the king
-    pieces = self.pieces.where(color: opposing_color, active: true).to_a
-    pieces.each do |piece|
-      if piece.valid_move?(king.x, king.y)
-        threatening_pieces << piece
-      end
-    end
-    return threatening_pieces
-  end
-
-  def threatening_piece_may_be_captured_by_teammate?(color)
-    king = self.pieces.find_by(type: "King", color: color)
-    game = king.game
-    game_id = game.id
-    pieces = king.game.find_in_game(color: color, active: true).to_a
-    threatening_pieces = threatening_pieces?(color)
-
-    threatening_pieces.each do |threatening_piece|
-      pieces.each do |piece|
-        if piece.valid_move?(threatening_piece.x, threatening_piece.y)
-          return true
-        end
-      end
-    end
-    return false
-  end
-
-  def threatening_piece_may_be_blocked_by_teammate?(color)
-    threatening_pieces = threatening_pieces?(color)
-    king = self.pieces.find_by(type: "King", color: color)
-    teammate_pieces = self.pieces.where(color: color, active: true).where.not(type: "King").to_a
-
-    # if there is more than 1 threatening piece, it doesn't matter if you can block one of them
-    # because the other one(s) can capture the king on the next move
-    if threatening_pieces.length == 1
-      threatening_piece = threatening_pieces[0]
-
-      # check each capture path for this threatening piece
-      threatening_piece.capture_path.values.each do |path|
-
-        # find which path the king is on
-        if path.include?([king.x, king.y])
-          king_is_on_this_path = path
-        else
-          next
-        end
-
-        # go through each of pieces on your own team
-        teammate_pieces.each do |teammate_piece|
-
-          # go through each of the cells between the threatening piece and the king
-          king_is_on_this_path.each do |cell|
-
-            # if a teammate piece may move to any of the cells along this path, the threat CAN be BLOCKED
-            return true if teammate_piece.valid_move?(cell[0], cell[1])
-          end
-        end
-
-        # the threat cannot be blocked
-        return false
-      end
-    end
-
-    # there are more than 1 threatening pieces
-    return false
-  end
 end
