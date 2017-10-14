3c4993d8105dcecf3b62b0fc3a7c1469ed500c4f
Kevin Kawai <kevin.fredricks@gmail.com>
2017-10-14 08:16:03 +0000

    changed redis settings for team heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.web_socket_server_url = "wss://websockets-test-kevin-kawai.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://websockets-test-kevin-kawai.herokuapp.com', 'http://websockets-test-kevin-kawai.herokuapp.com']
+  config.web_socket_server_url = "wss://theknightclass.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://theknightclass.herokuapp.com', 'http://theknightclass.herokuapp.com']
 
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'theknightclass.herokuapp.com' }
